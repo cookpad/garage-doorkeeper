7995c2b8eb2c591e8101cf15984970f98307a6c6
Taiki ONO <taiki-ono@cookpad.com>
2015-02-08 21:14:24 +0900

    init

---
+++ lib/garage/doorkeeper.rb
@@ -0,0 +1,7 @@
+require "garage/doorkeeper/version"
+
+module Garage
+  module Doorkeeper
+    # Your code goes here...
+  end
+end
---
+++ lib/garage/doorkeeper/version.rb
@@ -0,0 +1,5 @@
+module Garage
+  module Doorkeeper
+    VERSION = "0.0.1"
+  end
+end
