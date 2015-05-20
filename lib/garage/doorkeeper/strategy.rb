d637bd2ac6e8c8addae1c873f96274f3661b28e3
Taiki Ono <taiki-ono@cookpad.com>
2015-05-20 14:48:30 +0900

    Follow latest garage strategy interface

--- lib/garage/doorkeeper/strategy.rb
+++ lib/garage/doorkeeper/strategy.rb
@@ -1,19 +1,27 @@
 module Garage
   module Doorkeeper
+    # doorkeeper_token has same interface with Garage::Strategy::AccessToken
     module Strategy
       extend ActiveSupport::Concern
 
+      # Garage::ControllerHelper already implements `doorkeeper_token`,
+      # so monkey patch here.
+      ::Doorkeeper::Helpers::Filter.class_eval do
+        alias_method :original_doorkeeper_token, :doorkeeper_token
+      end
+
       included do
         include ::Doorkeeper::Helpers::Filter
-        # doorkeeper_token has same interface with Garage::Strategy::AccessToken
-        alias_method :access_token, :doorkeeper_token
         alias_method :doorkeeper_unauthorized_render_options, :unauthorized_render_options
-
         doorkeeper_for :all
       end
 
       def verify_permission?
         true
+      end
+
+      def access_token
+        original_doorkeeper_token
       end
     end
   end
